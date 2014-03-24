809e9d7e95fa3c1d8b39951b5ca014a6076611a0
Gabelo <gabeloski15@gmail.com>
2014-03-24 09:31:42 -0600

    Arreglados los fallos para insertarla información y almacenamiento en el archivo

--- Agregar_amigos.c
+++ Agregar_amigos.c
@@ -9,27 +9,28 @@ struct usuario{
 
 int main()
 {
-	char nombre_usuario[]=" ";
-	char ip[]=" ";
+	char nombre_usuario[30];
+	char ip[20];
 	int port;
 	printf("Digite el nombre del usuario a agregar\n");
 	scanf("%s",nombre_usuario);
 	printf("\n");
+    printf("%s\n", nombre_usuario);
 	printf("Digite el ip del usuario:\n");
 	scanf("%s",ip);
-	printf("\n");
+    printf("%s\n",nombre_usuario);
+	printf("%s\n", ip);
 	printf("Digite el puerto de comunicacion");
 	scanf("%d",&port);
+    printf("%s, %s, %d\n", nombre_usuario, ip, port);
 	struct usuario temp;
     temp.nombre = nombre_usuario;
     temp.ipa = ip;
     temp.puerto = port;
 	printf("\n");
 	FILE *pointer;
-	pointer = fopen("amigos.txt", "r+");
-	fprintf(pointer, "%50s%20s%4d\n", temp.nombre, temp.ipa, temp.puerto);
-    //fputs(temp, pointer);
-	//fputs("\n",pointer);
+	pointer = fopen("amigos.txt", "a");
+	fprintf(pointer, "%30s%30s%4d\n", temp.nombre, temp.ipa, temp.puerto);
 	fclose (pointer);
 	return 0;
 }
